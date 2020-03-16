abb6d2215eb1adcfb4059e28b0b8b614d91ddf3d
Joan Pedro Oliveira de Souza <31076821+JoanPedro@users.noreply.github.com>
2020-03-16 10:49:59 -0300

    Atualiza para ES6+
    Alterações feita por dispositivo móvel.

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-var http = require('http');
-var fs = require('fs');
-var urlParser = require('url');
-var qlikAuth = require('qlik-auth');
+const http = require('http')
+const fs = require('fs')
+const urlParser = require('url')
+const qlikAuth = require('qlik-auth')
 
-var contentTypeMap = {
+const contentTypeMap = {
   "js":"text/javascript",
   "html":"text/html",
   "css":"text/css",
   "ttf":"font/ttf",
   "ico":"image/x-icon"
-};
+}
 
-var server = http.createServer(function(req, res){
+var server = http.createServer((req, res) => {
   switch(req.url){
     case "/":
-      serveFile("public/index.html", res);
+      serveFile("public/index.html", res)
       break;
     default:
       if(req.url.indexOf('authenticate?')!=-1){
@@ -24,33 +24,33 @@ var server = http.createServer(function(req, res){
           'Attributes': []
         }
 
-        var options = {
+        const options = {
          'Certificate': './client.pfx',
          'PassPhrase': ''
         }
-        qlikAuth.requestTicket(req, res, profile, options);
+        qlikAuth.requestTicket(req, res, profile, options)
       }
       else{
-        serveFile(req.url.replace(/\//,''), res);
+        serveFile(req.url.replace(/\//,''), res)
       }
       break;
   }
-}).listen(3000);
+}).listen(3000)
 
-console.log('Server running on port 3000');
+console.log('Server running on port 3000')
 
-function serveFile(url, res){
-  console.log(url.replace(/\//, ''));
-  var contentType = contentTypeMap[url.split('.').pop()];
-  console.log(contentType);
+const serveFile = (url, res) => {
+  console.log(url.replace(/\//, ''))
+  var contentType = contentTypeMap[url.split('.').pop()]
+  console.log(contentType)
   fs.readFile(url, function(err, data){
-    res.writeHead(200, {'Content-Type':contentType});
+    res.writeHead(200, {'Content-Type':contentType})
     if(contentType=="image/x-icon"){
-      res.write(""+data);
+      res.write(""+data)
     }
     else {
-      res.write(data);
+      res.write(data)
     }
-    res.end();
+    res.end()
   })
 }
